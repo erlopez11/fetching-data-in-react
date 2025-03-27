1b0d59ddd4ff00152819e9e7578ae941a954745f
erlopez11 <lopezelainy@gmail.com>
2025-03-27 15:40:37 -0500

    wip: implement weather service in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import './App.css';
+import * as weatherService from './services/weatherService';
 
 const App = () => {
-
+  const fetchData = async () => {
+    const data = await weatherService.show('Guatemala City');
+    console.log('Data:', data);
+  };
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
 }
 
